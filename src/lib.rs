6c505a2ab8eb2813a5dc6e7eaaf21d42f9e2c01e
Giovanni <7008900+sinkingsugar@users.noreply.github.com>
2021-12-27 15:30:56 +0900

    fix threading issues and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,43 @@
 extern crate chainblocks;
+extern crate edn_rs;
 
 use chainblocks::cbl_env;
 use chainblocks::types::ClonedVar;
+use chainblocks::types::Table;
+use edn_rs::{edn, Edn, Map};
+
+use std::sync::Once;
+
+static INIT: Once = Once::new();
+
+pub fn initialize() {
+  INIT.call_once(|| {
+    chainblocks::core::init();
+  });
+}
 
 #[test]
 fn main() {
-  chainblocks::core::init();
+  initialize();
+
   let res = cbl_env!(include_str!("test.edn")).unwrap();
   let res = <i64>::try_from(&res.0).unwrap();
   assert_eq!(res, 99);
+}
+
+#[test]
+fn edn_rs_usage() {
+  initialize();
+
+  let edn = edn!({:a 1 :b 2 :c 3});
+  let edn = edn.to_string();
+  assert_eq!(edn, "{:a 1, :b 2, :c 3, }");
+  let res = cbl_env!(edn).unwrap();
+  let mut res = <Table>::try_from(&res.0).unwrap();
+  let a = <i64>::try_from(res.get_fast_static("a\0")).unwrap();
+  assert_eq!(a, 1);
+  let b = <i64>::try_from(res.get_fast_static("b\0")).unwrap();
+  assert_eq!(b, 2);
+  let c = <i64>::try_from(res.get_fast_static("c\0")).unwrap();
+  assert_eq!(c, 3);
 }
