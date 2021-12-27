86a02755c123a76674ddd418e25d878e8374ee69
Giovanni <7008900+sinkingsugar@users.noreply.github.com>
2021-12-27 11:56:53 +0900

    fix native calls, add edn test

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 extern crate chainblocks;
 
 use chainblocks::cbl_env;
+use chainblocks::types::ClonedVar;
 
 #[test]
 fn main() {
   chainblocks::core::init();
-  cbl_env!("(println \"Hello, world!\")");
+  let res = cbl_env!(include_str!("test.edn")).unwrap();
+  let res = <i64>::try_from(&res.0).unwrap();
+  assert_eq!(res, 99);
 }
