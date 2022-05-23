583708f62a44cde3f580dfc76aad65d457c2f6d3
Giovanni <7008900+sinkingsugar@users.noreply.github.com>
2022-05-23 10:49:11 +0800

    rework program trait thing

--- app/src/main.rs
+++ app/src/main.rs
@@ -15,9 +15,13 @@ fn main() {
   let env = new_env();
   let main_node = Node::default();
 
-  let main: ClonedVar = cbl_env!(env, include_str!("main.edn")).unwrap();
-  let traits: Table = main.0.as_ref().try_into().expect("Traits Table");
-  let program = Program::distill(&traits).expect("Program");
+  // exec the script, notice we on purpose return nil to avoid varifying the trait inside!
+  // the reason being, varify will consume the blocks and null them out, which is not what we want
+  let _ = cbl_env!(env, concat!("(do ", include_str!("main.edn"), " nil)")).expect("main.edn");
+  // extract the trait we need
+  let program: ClonedVar = cbl_env!(env, "Program").expect("Program ClonedVar");
+  let program: Table = program.0.as_ref().try_into().expect("Program Table");
+  let _program = Program::distill(&program).expect("Program distill");
 
   // main_node.schedule(main_chain.0.try_into().unwrap());
 
--- library/src/program.rs
+++ library/src/program.rs
@@ -1,19 +1,17 @@
 use crate::ProtoTrait;
-use chainblocks::types::{BlockRef, Table};
+use chainblocks::types::{Table, Seq};
 
 pub struct Program {
-  pub on_rez: BlockRef,
-  pub on_derez: BlockRef,
-  pub advance: BlockRef,
+  pub on_rez: Seq,
+  pub on_derez: Seq,
+  pub advance: Seq,
 }
 
 impl ProtoTrait for Program {
   fn distill(traits: &Table) -> Result<Program, &'static str> {
-    let program: Table = traits.get_fast_static("Program\0").try_into()?;
-
-    let on_rez: BlockRef = program.get_fast_static("OnRez\0").try_into()?;
-    let on_derez: BlockRef = program.get_fast_static("OnDerez\0").try_into()?;
-    let advance: BlockRef = program.get_fast_static("Advance\0").try_into()?;
+    let on_rez: Seq = traits.get_fast_static("OnRez\0").try_into()?;
+    let on_derez: Seq = traits.get_fast_static("OnDerez\0").try_into()?;
+    let advance: Seq = traits.get_fast_static("Advance\0").try_into()?;
 
     Ok(Program {
       on_rez,
