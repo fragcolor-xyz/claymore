8391306b05414296503d4c5e6c796b9d91a110bf
Giovanni <7008900+sinkingsugar@users.noreply.github.com>
2022-05-23 13:21:13 +0800

    run the main program properly (we are experimenting with traits here in general)

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,7 +3,7 @@ use std::time::{Duration, Instant};
 use chainblocks::cbl_env;
 use chainblocks::cblisp::new_env;
 use chainblocks::core::{init, sleep};
-use chainblocks::types::{ClonedVar, Node, Table};
+use chainblocks::types::{ClonedVar, Node, Table, Chain, BlockRef};
 use claylib::program::Program;
 use claylib::ProtoTrait;
 
@@ -21,9 +21,23 @@ fn main() {
   // extract the trait we need
   let program: ClonedVar = cbl_env!(env, "Program").expect("Program ClonedVar");
   let program: Table = program.0.as_ref().try_into().expect("Program Table");
-  let _program = Program::distill(&program).expect("Program distill");
+  let program = Program::distill(&program).expect("Program distill");
 
-  // main_node.schedule(main_chain.0.try_into().unwrap());
+  let main_chain = Chain::default();
+  main_chain.set_looped(true);
+  main_chain.set_name("Main Claymore Chain");
+
+  if let Some(on_rez) = &program.on_rez {
+    let once = BlockRef::create("Once");
+    once.set_parameter(0, on_rez.into());
+    main_chain.add_block(once);
+  }
+
+  for block in program.advance {
+    main_chain.add_block(block.try_into().expect("Advance Block"));
+  }
+
+  main_node.schedule(main_chain.0);
 
   let quantum = Duration::from_secs_f64(QUANTUM_TIME);
   let zero = Duration::from_secs_f64(0.0);
--- library/src/program.rs
+++ library/src/program.rs
@@ -1,16 +1,24 @@
 use crate::ProtoTrait;
-use chainblocks::types::{Table, Seq};
+use chainblocks::types::{Seq, Table};
 
 pub struct Program {
-  pub on_rez: Seq,
-  pub on_derez: Seq,
+  pub on_rez: Option<Seq>,
+  pub on_derez: Option<Seq>,
   pub advance: Seq,
 }
 
 impl ProtoTrait for Program {
   fn distill(traits: &Table) -> Result<Program, &'static str> {
-    let on_rez: Seq = traits.get_fast_static("OnRez\0").try_into()?;
-    let on_derez: Seq = traits.get_fast_static("OnDerez\0").try_into()?;
+    let on_rez: Option<Seq> = if let Ok(on_rez) = traits.get_fast_static("OnRez\0").try_into() {
+      Some(on_rez)
+    } else {
+      None
+    };
+    let on_derez: Option<Seq> = if let Ok(on_derez) = traits.get_fast_static("OnDerez\0").try_into() {
+      Some(on_derez)
+    } else {
+      None
+    };
     let advance: Seq = traits.get_fast_static("Advance\0").try_into()?;
 
     Ok(Program {
