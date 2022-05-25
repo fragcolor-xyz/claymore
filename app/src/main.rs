c067fa5758cc2726a31c1b9e2f5a21c5dc0585e1
Giovanni <7008900+sinkingsugar@users.noreply.github.com>
2022-05-25 14:58:04 +0800

    better simpler usage of traits directly from cb

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,9 +3,7 @@ use std::time::{Duration, Instant};
 use chainblocks::cbl_env;
 use chainblocks::cblisp::new_env;
 use chainblocks::core::{init, sleep};
-use chainblocks::types::{ClonedVar, Node, Table, Chain, BlockRef};
-use claylib::program::Program;
-use claylib::ProtoTrait;
+use chainblocks::types::{ClonedVar, Node, ChainRef};
 
 const QUANTUM_TIME: f64 = 1.0 / 120.0;
 
@@ -18,40 +16,9 @@ fn main() {
   // exec the script, notice we on purpose return nil to avoid varifying the trait inside!
   // the reason being, varify will consume the blocks and null them out, which is not what we want
   let _ = cbl_env!(env, concat!("(do ", include_str!("main.edn"), " nil)")).expect("main.edn");
-  // extract the trait we need, this step consumes the blocks from the script env, Program will be invalid after!
-  let program: ClonedVar = cbl_env!(env, "(do (println Program) Program)").expect("Program ClonedVar");
-  let program: Table = program.0.as_ref().try_into().expect("Program Table");
-  let program = Program::distill(&program).expect("Program distill");
-
-  let main_chain = Chain::default();
-  main_chain.set_name("Main Claymore Chain");
-
-  if let Some(on_rez) = program.on_rez {
-    for block in on_rez {
-      main_chain.add_block(block.try_into().expect("OnRez Block"));
-    }
-  }
-
-  let looped_chain = Chain::default();
-  looped_chain.set_name("Claymore Main Loop Chain");
-  looped_chain.set_looped(true);
-
-  for block in program.advance {
-    looped_chain.add_block(block.try_into().expect("Advance Block"));
-  }
-
-  let dispatch_loop = BlockRef::create("Dispatch");
-  dispatch_loop.set_parameter(0, looped_chain.0.into());
-
-  main_chain.add_block(dispatch_loop);
-
-  if let Some(on_derez) = program.on_derez {
-    for block in on_derez {
-      main_chain.add_block(block.try_into().expect("OnDerez Block"));
-    }
-  }
-
-  main_node.schedule(main_chain.0);
+  let loop_chain = cbl_env!(env, include_str!("loop.edn")).expect("loop.edn");
+  let loop_chain: ChainRef = loop_chain.0.try_into().expect("loop.edn ChainRef");
+  main_node.schedule(loop_chain);
 
   let quantum = Duration::from_secs_f64(QUANTUM_TIME);
   let zero = Duration::from_secs_f64(0.0);
